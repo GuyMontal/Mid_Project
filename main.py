d580c802f304aa0b5660095bde1ec77363676144
Guy Montal <guymontal@gmail.com>
2024-03-23 15:47:14 +0200

    Adding plotting

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-import json
 
 # A ticker is one stock
-tickers = ["msft","aapl"]
+tickers = ['NVDA','ARM']
 
-#pulling data from api directly into pandas df
-for ticker in tickers:
-    stockData = yf.Ticker(ticker).history(period = "5d")
-    stockDataClose = yf.Ticker(ticker).history(period = "5d").Close
-    print(f"The data for {ticker} is:\n {stockData}\n")
+# Fetch historical data
+stocksData = yf.download(tickers, period = "1mo")
 
+# Select close prices and volumes
+close_prices = stocksData['Adj Close'] / stocksData['Adj Close'].mean()
+volumes = stocksData['Volume']
+
+# Plot close prices
+plt.figure(figsize=(10, 10))
+close_prices.plot()
+plt.title('Close Prices')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend(tickers)
+#plt.show()
+
+# Plot volumes
+plt.figure(figsize=(10, 5))
+volumes.plot()
+plt.title('Volumes Traded')
+plt.xlabel('Date')
+plt.ylabel('Volume')
+plt.legend(tickers)
+#plt.show()
+
+# Calculate daily returns
+daily_returns = close_prices.pct_change()
+
+# Plot daily returns
+plt.figure(figsize=(10, 5))
+daily_returns.plot()
+plt.title('Daily Returns')
+plt.xlabel('Date')
+plt.ylabel('Return')
+plt.legend(tickers)
+#plt.show()
