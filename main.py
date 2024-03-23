9d3feea72ecffb3edd6980a05e33835965b726f0
Guy Montal <guymontal@gmail.com>
2024-03-23 15:45:30 +0200

    Stating plotting with volume and closing prices

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
+import json
 
+# A ticker is one stock
 tickers = ["msft","aapl"]
 
+#pulling data from api directly into pandas df
+for ticker in tickers:
+    stockData = yf.Ticker(ticker).history(period = "5d")
+    stockDataClose = yf.Ticker(ticker).history(period = "5d").Close
+    print(f"The data for {ticker} is:\n {stockData}\n")
 
-stocksData = yf.Ticker(tickers[0]).history(period = "1d")
-
-print(stocksData)
